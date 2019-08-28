1212232b6e5fcb6bfa422bced5874e7d546c9907
Tomasz Rycerz <tomryc@gmail.com>
2019-08-28 20:34:51 +0100

    Adding Query Method Examples

--- src/main/java/com/tomryc/controllers/IndexController.java
+++ src/main/java/com/tomryc/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package com.tomryc.controllers;
 
+import com.tomryc.domain.Category;
+import com.tomryc.domain.UnitOfMeasure;
+import com.tomryc.repositories.CategoryRepository;
+import com.tomryc.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
--- src/main/java/com/tomryc/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/tomryc/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.tomryc.repositories;
 import com.tomryc.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
