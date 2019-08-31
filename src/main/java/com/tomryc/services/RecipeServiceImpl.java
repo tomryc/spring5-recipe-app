03711870e1adf8d88ef1bf8fd400cf08e14f04ad
Tomasz Rycerz <tomryc@gmail.com>
2019-08-31 13:55:33 +0100

    refactoring for project lombok

--- src/main/java/com/tomryc/services/RecipeServiceImpl.java
+++ src/main/java/com/tomryc/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.tomryc.services;
 
 import com.tomryc.domain.Recipe;
 import com.tomryc.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService{
 
@@ -18,6 +20,8 @@ public class RecipeServiceImpl implements RecipeService{
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
