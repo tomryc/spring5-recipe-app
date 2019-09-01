2d7bd650da066c1d6ae70dc576a0f41fea9cc183
Tomasz Rycerz <tomryc@gmail.com>
2019-09-01 13:56:37 +0100

    testing controller

---
+++ src/test/java/com/tomryc/controllers/IndexControllerTest.java
@@ -0,0 +1,38 @@
+package com.tomryc.controllers;
+
+import com.tomryc.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class IndexControllerTest {
+
+    private IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() throws Exception{
+        String output = indexController.getIndexPage(model);
+
+        assertEquals("index", output);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+
+    }
+}
