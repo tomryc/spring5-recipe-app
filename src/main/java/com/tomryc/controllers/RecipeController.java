3a7eaa77b02f01b96b374b76b2ed106236242f2b
Tomasz Rycerz <tomryc@gmail.com>
2019-10-29 10:36:20 +0000

    adding assignment for Number Format Exception

--- src/main/java/com/tomryc/controllers/RecipeController.java
+++ src/main/java/com/tomryc/controllers/RecipeController.java
@@ -70,4 +70,19 @@ public class RecipeController {
         return modelAndView;
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleBadRequest(Exception exception){
+        log.error("Handling bad request exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
+
+
+        return modelAndView;
+    }
+
 }
